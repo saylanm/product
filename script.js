9cdd17d85d4288e7977fab37bf91b2cd65858ccc
semen <i@schanel.ru>
2024-12-28 12:24:27 +0400

    new_a

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const swiper = new Swiper('.swiper', {
-        slidesPerView: 1.2,
-        spaceBetween: 10,
-        loop: true,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-    });
+    let swiperInstance;
+
+    const initSwiper = () => {
+        if (window.innerWidth <= 768 && !swiperInstance) { // Проверяем, мобильная ли версия
+            swiperInstance = new Swiper('.swiper', {
+                slidesPerView: 1.2,
+                spaceBetween: 10,
+                loop: true,
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true,
+                },
+            });
+        } else if (window.innerWidth > 768 && swiperInstance) { // Отключаем Swiper на большом экране
+            swiperInstance.destroy(true, true);
+            swiperInstance = null;
+        }
+    };
+
+    // Инициализация при загрузке страницы
+    initSwiper();
+
+    // Отслеживаем изменение размера окна
+    window.addEventListener('resize', initSwiper);
 });
 
