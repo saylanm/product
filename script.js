2cf5749c670841a0efe2c00fbe35bc5286599d78
semen <i@schanel.ru>
2024-12-29 23:49:05 +0400

    new_js_event

--- script.js
+++ script.js
@@ -18,10 +18,36 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     };
 
+
+
     // Инициализация при загрузке страницы
     initSwiper();
 
     // Отслеживаем изменение размера окна
     window.addEventListener('resize', initSwiper);
 });
+
+var button = document.querySelector('.open');
+var button_closed = document.querySelector('.closesd');
+var open = document.querySelector('.open');
+var content = document.querySelector('.modal');
+var img = document.querySelector('.img');
+
+button.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    content.classList.add('modal-show')
+    button.classList.add('modal')
+    open.classList.add('modal')
+    img.classList.add('modal')
+    console.log('отрывается')
+
+})
+
+button_closed.addEventListener('click', function() {
+    content.classList.remove('modal-show')
+    button.classList.remove('modal')
+    open.classList.remove('modal')
+    img.classList.remove('modal')
+    console.log('закрывается')
+})
 
