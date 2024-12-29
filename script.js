781ba5509f011eeea430b2f20e05c2e6391d08a6
semen <i@schanel.ru>
2024-12-30 00:18:41 +0400

    new_js

--- script.js
+++ script.js
@@ -29,25 +29,31 @@ document.addEventListener('DOMContentLoaded', () => {
 
 var button = document.querySelector('.open');
 var button_closed = document.querySelector('.closesd');
-var open = document.querySelector('.open');
+var close = document.querySelector('.close');
 var content = document.querySelector('.modal');
-var img = document.querySelector('.img');
+var img = document.querySelector('.close');
+var image = document.querySelector('.img');
+
 
 button.addEventListener('click', function(evt) {
     evt.preventDefault();
-    content.classList.add('modal-show')
-    button.classList.add('modal')
-    open.classList.add('modal')
-    img.classList.add('modal')
-    console.log('отрывается')
+    content.classList.add('modal-show');
+    button.classList.add('modal');
+    img.classList.add('modal');
+    button_closed.classList.remove('modal');
+    img.classList.remove('modal');
+    image.classList.add('modal');
+    console.log('отрывается');
 
 })
 
 button_closed.addEventListener('click', function() {
-    content.classList.remove('modal-show')
-    button.classList.remove('modal')
-    open.classList.remove('modal')
-    img.classList.remove('modal')
-    console.log('закрывается')
+    content.classList.remove('modal-show');
+    button.classList.remove('modal');
+    img.classList.remove('modal');
+    button_closed.classList.add('modal');
+    image.classList.remove('modal');
+    close.classList.add('modal');
+    console.log('закрывается');
 })
 
